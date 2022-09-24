cfc39908c91bd84f0291526c1c3ac5653188714f
RLC02 <codor417@gmail.com>
2022-09-24 20:28:47 -0300

    envio calculadors

---
+++ calculadora/principal.js
@@ -0,0 +1,64 @@
+class Calculadora{
+
+    somar(n1, n2){
+        return n1+n2
+    }
+
+    subtrair(n1, n2){
+        return n1-n2
+    }
+    multiplicar(n1, n2){
+        return n1*n2
+    }
+    dividir (n1, n2){
+        return n1/n2
+    }
+}
+    function Somar() {
+    n1 = Number
+    n2 = Number
+    let minhacalculadora = new Calculadora();
+    var num1 = document.querySelector('input#num1')
+    var num2 = document.querySelector('input#num2')
+    var n1 = Number(num1.value)
+    var n2 = Number(num2.value)
+    var calculo = document.querySelector('div#calculo')
+    let resultadodaminhasoma = minhacalculadora.somar(n1,n2)  
+    calculo.innerHTML = `${n1} + ${n2} = ${resultadodaminhasoma}`
+    }
+    function Subtrair() {
+        n1 = Number
+        n2 = Number
+        let minhacalculadora = new Calculadora();
+        var num1 = document.querySelector('input#num1')
+        var num2 = document.querySelector('input#num2')
+        var n1 = Number(num1.value)
+        var n2 = Number(num2.value)
+        var calculo = document.querySelector('div#calculo')
+        let resultadodaminhasubtracao = minhacalculadora.subtrair(n1,n2)  
+        calculo.innerHTML = `${n1} - ${n2} = ${resultadodaminhasubtracao}`
+        }
+        function Multiplicar() {
+            n1 = Number
+            n2 = Number
+            let minhacalculadora = new Calculadora();
+            var num1 = document.querySelector('input#num1')
+            var num2 = document.querySelector('input#num2')
+            var n1 = Number(num1.value)
+            var n2 = Number(num2.value)
+            var calculo = document.querySelector('div#calculo')
+            let resultadodaminhamultiplicacao = minhacalculadora.multiplicar(n1,n2)  
+            calculo.innerHTML = `${n1} * ${n2} = ${resultadodaminhamultiplicacao}`
+            }   
+            function Dividir() {
+                n1 = Number
+                n2 = Number
+                let minhacalculadora = new Calculadora();
+                var num1 = document.querySelector('input#num1')
+                var num2 = document.querySelector('input#num2')
+                var n1 = Number(num1.value)
+                var n2 = Number(num2.value)
+                var calculo = document.querySelector('div#calculo')
+                let resultadodaminhadivisao= minhacalculadora.dividir(n1,n2)  
+                calculo.innerHTML = `${n1} / ${n2} = ${resultadodaminhadivisao}`
+                } 
